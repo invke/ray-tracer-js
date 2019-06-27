5078632c725d42d658dcf8df66c9b55d360d63b5
Montgomery Anderson <montgomery@boost.co.nz>
2019-06-28 10:42:16 +1200

    Implemented simple yarn lint script using eslint and config

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+      preload: path.join(__dirname, 'preload.js'),
+    },
   })
 
   // and load the index.html of the app.
