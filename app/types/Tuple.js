49bf264b3a1d4bf720e9f2e66b70c9be2b0163cc
Montgomery Anderson <montgomery@boost.co.nz>
2019-06-28 14:04:48 +1200

    Implemented tuple point, vector and areEquivalent static methods

--- app/types/Tuple.js
+++ app/types/Tuple.js
@@ -1,3 +1,5 @@
+import { EPSILON } from 'app/constants'
+
 export default class Tuple {
   constructor(x, y, z, w) {
     this.x = x
@@ -9,4 +11,17 @@ export default class Tuple {
   isPoint() { return this.w == 1.0 }
 
   isVector() { return !this.isPoint() }
+
+  static Point(x, y, z) { return new Tuple(x, y, z, 1.0) }
+
+  static Vector(x, y, z) { return new Tuple(x, y, z, 0.0) }
+
+  static AreEquivalent(oneTuple, anotherTuple) {
+    return (
+      Math.abs(oneTuple.x - anotherTuple.x) < EPSILON &&
+      Math.abs(oneTuple.y - anotherTuple.y) < EPSILON &&
+      Math.abs(oneTuple.z - anotherTuple.z) < EPSILON &&
+      Math.abs(oneTuple.w - anotherTuple.w) < EPSILON
+    )
+  }
 }
--- spec/types/Tuple.spec.js
+++ spec/types/Tuple.spec.js
@@ -45,4 +45,59 @@ describe('Tuple', () => {
       expect(tuple.isVector()).toBe(true)
     })
   })
+
+  describe('static methods', () => {
+
+    describe('#Point', () => {
+      beforeEach(() => tuple = Tuple.Point(4.3, -4.2, 3.1))
+
+      it('returns a point tuple', () => {
+        expect(tuple.isPoint()).toBe(true)
+      })
+
+      it('assigns axis\'', () => {
+        expect(tuple.x).toEqual(4.3)
+        expect(tuple.y).toEqual(-4.2)
+        expect(tuple.z).toEqual(3.1)
+      })
+    })
+
+    describe('#Vector', () => {
+      beforeEach(() => tuple = Tuple.Vector(4.3, -4.2, 3.1))
+
+      it('returns a vector tuple', () => {
+        expect(tuple.isVector()).toBe(true)
+      })
+
+      it('assigns axis\'', () => {
+        expect(tuple.x).toEqual(4.3)
+        expect(tuple.y).toEqual(-4.2)
+        expect(tuple.z).toEqual(3.1)
+      })
+    })
+
+    describe('#AreEquivalent', () => {
+
+      it('returns true if the x, y, z and are both points', () => {
+        expect(
+          Tuple.AreEquivalent(
+            new Tuple(4.3, -4.2, 3.1, 1.0),
+            new Tuple(4.3, -4.2, 3.1, 1.0)
+          )
+        ).toBe(true)
+      })
+
+      it('returns false if the x, y, z are equal but one is a point the other a vector', () => {
+        expect(
+          Tuple.AreEquivalent(
+            new Tuple(4.3, -4.2, 3.1, 1.0),
+            new Tuple(4.3, -4.2, 3.1, 0.0)
+          )
+        ).toBe(false)
+      })
+
+    })
+
+  })
+
 })
