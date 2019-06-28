82836e22681ac625fbbf13ba2cae84650042369e
Montgomery Anderson <montgomery@boost.co.nz>
2019-06-28 13:49:56 +1200

    Added in the second set of tuple specs

--- spec/types/Tuple.spec.js
+++ spec/types/Tuple.spec.js
@@ -4,7 +4,7 @@ import Tuple from 'app/types/Tuple'
 describe('Tuple', () => {
   let tuple
 
-  describe('a tuple with w=1.0 is a point', () => {
+  describe('with w = 1.0', () => {
     beforeEach(() => {
       tuple = new Tuple(4.3, -4.2, 3.1, 1.0)
     })
@@ -22,6 +22,27 @@ describe('Tuple', () => {
 
     it('is not a vector', () => {
       expect(tuple.isVector()).toBe(false)
+    })
+  })
+
+  describe('with w = 0.0', () => {
+    beforeEach(() => {
+      tuple = new Tuple(4.3, -4.2, 3.1, 0.0)
+    })
+
+    it('assigns the axis\'', () => {
+      expect(tuple.x).toEqual(4.3)
+      expect(tuple.y).toEqual(-4.2)
+      expect(tuple.z).toEqual(3.1)
+      expect(tuple.w).toEqual(0.0)
+    })
+
+    it('is not a point', () => {
+      expect(tuple.isPoint()).toBe(false)
+    })
+
+    it('is a vector', () => {
+      expect(tuple.isVector()).toBe(true)
     })
   })
 })
