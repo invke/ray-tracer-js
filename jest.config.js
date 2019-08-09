b356de607e9949658544ed74ef6b4d23e0d08363
Montgomery Anderson <montgomery@boost.co.nz>
2019-08-09 12:20:11 +1200

    Fixed eslint not pointing to the right location and reluctantly linted the jest config

--- jest.config.js
+++ jest.config.js
@@ -62,14 +62,14 @@ module.exports = {
 
   // An array of directory names to be searched recursively up from the requiring module's location
   moduleDirectories: [
-    "node_modules",
-    "<rootDir>"
+    'node_modules',
+    '<rootDir>',
   ],
 
   // An array of file extensions your modules use
   moduleFileExtensions: [
-    "js",
-    "json",
+    'js',
+    'json',
   //   "jsx",
   //   "ts",
   //   "tsx",
@@ -114,7 +114,7 @@ module.exports = {
 
   // A list of paths to directories that Jest should use to search for files in
   roots: [
-    "<rootDir>/spec"
+    '<rootDir>/spec',
   ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
@@ -125,14 +125,14 @@ module.exports = {
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: [
-    "<rootDir>/spec/support/customMatchers.js"
+    '<rootDir>/spec/support/customMatchers.js',
   ],
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "node",
+  testEnvironment: 'node',
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -188,4 +188,4 @@ module.exports = {
 
   // Whether to use watchman for file crawling
   // watchman: true,
-};
+}
